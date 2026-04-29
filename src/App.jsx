7e7a8195522c1f8922d11b0caa7286b1ebda8daa
Prasun <prasunsen.68@gmail.com>
2026-04-29 23:10:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+export default function App(){
+return(
+<div className="min-h-screen bg-black text-white flex items-center justify-center">
+<h1 className="text-3xl font-bold">Dashboard Working 🚀</h1>
+</div>
+)}
